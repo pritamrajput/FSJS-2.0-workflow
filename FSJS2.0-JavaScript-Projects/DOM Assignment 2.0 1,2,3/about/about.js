af63f5818bc8faa75ce7fe0588f0ac7372929327
Pritam Chauhan <rajputpritam619@gmail.com>
2023-01-31 16:46:16 +0530

    DOM project2 task1 Done

--- FSJS2.0-JavaScript-Projects/DOM Assignment 2.0 1,2,3/about/about.js
+++ FSJS2.0-JavaScript-Projects/DOM Assignment 2.0 1,2,3/about/about.js
@@ -11,16 +11,16 @@ accordian.forEach((element) => {
   });
 });
 
-// let ulList = document.querySelector('ul');
-// console.log(ulList);
-// let listInside = ulList.getElementsByTagName('li');
-// console.log(listInside);
-// listInside[2].textContent = "Projects"
-// for(let i = 0;i<accordian.length;i++){
-//   let elementHeading = accordian[i];
-//   elementHeading.style.backgroundColor = '#dadaf8';
-// }
-// for(let i = 2 ; i<accordian.length;i++){
-//   let nextElement = accordian[i].nextElementSibling
-//   nextElement.style.display = 'block';
-// }
+let ulList = document.querySelector('ul');
+console.log(ulList);
+let listInside = ulList.getElementsByTagName('li');
+console.log(listInside);
+listInside[2].textContent = "Projects"
+for(let i = 0;i<accordian.length;i++){
+  let elementHeading = accordian[i];
+  elementHeading.style.backgroundColor = '#dadaf8';
+}
+for(let i = 2 ; i<accordian.length;i++){
+  let nextElement = accordian[i].nextElementSibling
+  nextElement.style.display = 'block';
+}
--- FSJS2.0-JavaScript-Projects/DOM Assignment 2.0 1,2,3/secondAssignmentImage/readme.md
+++ FSJS2.0-JavaScript-Projects/DOM Assignment 2.0 1,2,3/secondAssignmentImage/readme.md
@@ -1,3 +1,22 @@
 # Take 1
 ## Original Page
 ![Image](aboutOriginal.PNG)
+## Achieve the following with DOM manipulation
+![Image](task1Output.png)
+## javaScript code
+```
+'use strict';
+let ulList = document.querySelector('ul');
+console.log(ulList);
+let listInside = ulList.getElementsByTagName('li');
+console.log(listInside);
+listInside[2].textContent = "Projects"
+for(let i = 0;i<accordian.length;i++){
+  let elementHeading = accordian[i];
+  elementHeading.style.backgroundColor = '#dadaf8';
+}
+for(let i = 2 ; i<accordian.length;i++){
+  let nextElement = accordian[i].nextElementSibling
+  nextElement.style.display = 'block';
+}
+```
