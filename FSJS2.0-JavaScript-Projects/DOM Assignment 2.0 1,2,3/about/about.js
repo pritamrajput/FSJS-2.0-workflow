c6b778d257f5089637a4da40f1f14632d67e9af5
Pritam Chauhan <rajputpritam619@gmail.com>
2023-01-31 20:36:01 +0530

    DOM Project2 Task 2 Done

--- FSJS2.0-JavaScript-Projects/DOM Assignment 2.0 1,2,3/about/about.js
+++ FSJS2.0-JavaScript-Projects/DOM Assignment 2.0 1,2,3/about/about.js
@@ -11,16 +11,42 @@ accordian.forEach((element) => {
   });
 });
 
-let ulList = document.querySelector('ul');
-console.log(ulList);
-let listInside = ulList.getElementsByTagName('li');
-console.log(listInside);
-listInside[2].textContent = "Projects"
+// let ulList = document.querySelector('ul');
+// console.log(ulList);
+// let listInside = ulList.getElementsByTagName('li');
+// console.log(listInside);
+// listInside[2].textContent = "Projects"
+// for(let i = 0;i<accordian.length;i++){
+//   let elementHeading = accordian[i];
+//   elementHeading.style.backgroundColor = '#dadaf8';
+// }
+// for(let i = 2 ; i<accordian.length;i++){
+//   let nextElement = accordian[i].nextElementSibling
+//   nextElement.style.display = 'block';
+// }
+
+let main =  document.querySelector('.accordian-wrapper');
+let divAdded = document.createElement('div');
+divAdded.classList.add('accordian');
+// divAdded.getElementsByTagName('h3').style.backgroundColor = "#dadaf8"
+divAdded.innerHTML = `<h3>Skills</h3><p>I posses a very good command over the Full Stack Development technologies like MERN which can be seen in my work over the github.</p>`;
+main.appendChild(divAdded);
+
+divAdded.addEventListener('click', function(){
+ let para = divAdded.getElementsByTagName('p');
+ if(para[0].style.display === 'block'){
+  para[0].style.display ='none'
+ }
+ else{
+  para[0].style.display = 'block'
+ }
+})
+
+// color change
 for(let i = 0;i<accordian.length;i++){
-  let elementHeading = accordian[i];
-  elementHeading.style.backgroundColor = '#dadaf8';
-}
-for(let i = 2 ; i<accordian.length;i++){
-  let nextElement = accordian[i].nextElementSibling
-  nextElement.style.display = 'block';
-}
+    let elementHeading = accordian[i];
+    elementHeading.style.backgroundColor = '#dadaf8';
+  }
+
+ let headingColor =  divAdded.getElementsByTagName('h3');
+ headingColor[0].style.backgroundColor = '#dadaf8';
--- FSJS2.0-JavaScript-Projects/DOM Assignment 2.0 1,2,3/secondAssignmentImage/readme.md
+++ FSJS2.0-JavaScript-Projects/DOM Assignment 2.0 1,2,3/secondAssignmentImage/readme.md
@@ -20,3 +20,37 @@ for(let i = 2 ; i<accordian.length;i++){
   nextElement.style.display = 'block';
 }
 ```
+# Task 2
+## Original Page
+![Image](aboutOriginal.PNG)
+## Achieve the following with DOM manipulation
+![Image](./task2Output.png)
+## javaScript code
+```js
+'use strict';
+let main =  document.querySelector('.accordian-wrapper');
+let divAdded = document.createElement('div');
+divAdded.classList.add('accordian');
+// divAdded.getElementsByTagName('h3').style.backgroundColor = "#dadaf8"
+divAdded.innerHTML = `<h3>Skills</h3><p>I posses a very good command over the Full Stack Development technologies like MERN which can be seen in my work over the github.</p>`;
+main.appendChild(divAdded);
+
+divAdded.addEventListener('click', function(){
+ let para = divAdded.getElementsByTagName('p');
+ if(para[0].style.display === 'block'){
+  para[0].style.display ='none'
+ }
+ else{
+  para[0].style.display = 'block'
+ }
+})
+
+// color change
+for(let i = 0;i<accordian.length;i++){
+    let elementHeading = accordian[i];
+    elementHeading.style.backgroundColor = '#dadaf8';
+  }
+
+ let headingColor =  divAdded.getElementsByTagName('h3');
+ headingColor[0].style.backgroundColor = '#dadaf8';
+```
