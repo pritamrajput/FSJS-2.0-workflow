d458cfa192b9f37e3cad9cb7225dcc284f2c2744
Pritam Chauhan <rajputpritam619@gmail.com>
2023-01-31 12:31:24 +0530

    DOM Project1 task 3 Done

--- FSJS2.0-JavaScript-Projects/DOM Assignment 2.0 1,2,3/firstAssignmentImage/readme.md
+++ FSJS2.0-JavaScript-Projects/DOM Assignment 2.0 1,2,3/firstAssignmentImage/readme.md
@@ -40,3 +40,24 @@ let input = document.querySelector('.search-field');
 input.innerHTML = ` <input type="text" placeholder="Search My Projects" />
 <button>Search</button>`
 ```
+
+# Task 3
+## original page
+![alt text](./original.PNG)
+## Achieve the following with DOM manipulation
+![alt text](./task3Output.png)
+
+## javascript code
+```js
+'use strict';
+let list = document.querySelector('ul');
+let listInside = list.getElementsByTagName('li');
+console.log(listInside);
+listInside[2].innerText = "Projects";
+
+let section = document.querySelector('.hero-left-section')
+let para1 = section.getElementsByTagName('p');
+let spanAll = para1[0].getElementsByTagName('span');
+spanAll[1].innerText = "an employee"
+spanAll[2].innerText = "iNeuron Intelligence Pvt Ltd"
+```
--- FSJS2.0-JavaScript-Projects/DOM Assignment 2.0 1,2,3/script.js
+++ FSJS2.0-JavaScript-Projects/DOM Assignment 2.0 1,2,3/script.js
@@ -9,14 +9,24 @@
 // let list2 = document.querySelectorAll('ul');
 // list2[1].innerHTML = "";
 
+// let list = document.querySelector('ul');
+// list.innerHTML = `<li><a href="./index.html">Home</a></li>
+//  <li> <a href="./about/about.html">About</a> </li>
+// <li><a href="./contact/contact.html">Projects</a></li>`
+
+// let list2 = document.querySelectorAll('ul');
+// list2[1].innerHTML = "";
+
+// let input = document.querySelector('.search-field');
+// input.innerHTML = ` <input type="text" placeholder="Search My Projects" />
+// <button>Search</button>`
 let list = document.querySelector('ul');
-list.innerHTML = `<li><a href="./index.html">Home</a></li>
- <li> <a href="./about/about.html">About</a> </li>
-<li><a href="./contact/contact.html">Projects</a></li>`
+let listInside = list.getElementsByTagName('li');
+console.log(listInside);
+listInside[2].innerText = "Projects";
 
-let list2 = document.querySelectorAll('ul');
-list2[1].innerHTML = "";
-
-let input = document.querySelector('.search-field');
-input.innerHTML = ` <input type="text" placeholder="Search My Projects" />
-<button>Search</button>`
+let section = document.querySelector('.hero-left-section')
+let para1 = section.getElementsByTagName('p');
+let spanAll = para1[0].getElementsByTagName('span');
+spanAll[1].innerText = "an employee"
+spanAll[2].innerText = "iNeuron Intelligence Pvt Ltd"
