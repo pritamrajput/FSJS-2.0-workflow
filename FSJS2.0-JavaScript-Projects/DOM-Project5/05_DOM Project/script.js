00ece842a225c66fc67154ae3d57e5c5e0ccd12a
Pritam Chauhan <rajputpritam619@gmail.com>
2023-02-08 22:39:23 +0530

    few changes made

--- FSJS2.0-JavaScript-Projects/DOM-Project5/05_DOM Project/script.js
+++ FSJS2.0-JavaScript-Projects/DOM-Project5/05_DOM Project/script.js
@@ -1,1 +1,19 @@
 "use strict";
+
+//Recipes list
+const tagCont = document.querySelector('.tags-container')
+const tagContNext = tagCont.firstElementChild.nextElementSibling;
+console.log(tagContNext);
+const aElement = document.createElement('a')
+aElement.innerText = "Chinese(7)"
+aElement.href = "#"
+tagContNext.appendChild(aElement)
+
+// Navbar button
+const proBTN = document.querySelector('.nav-center');
+const lastChild = proBTN.lastElementChild;
+const btnHref = document.createElement('a');
+btnHref.innerText = "Pro Subscription";
+btnHref.href = "#"
+btnHref.classList.add('btn')
+lastChild.appendChild(btnHref);
