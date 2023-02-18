61e5df14ba4a252859f9ebc5dc4621c6c5e484b8
Pritam Chauhan <rajputpritam619@gmail.com>
2023-02-18 22:46:20 +0530

    new code added

---
+++ DOM-Side-Projects/Project03/script.js
@@ -0,0 +1,15 @@
+'use strict';
+const form = document.querySelector(".input__field");
+const getFloors = document.querySelector(".input__floor");
+const getLifts= document.querySelector(".input__lift");
+
+form.addEventListener("submit", function(e){
+  e.preventDefault();
+  const myFloors = getFloors.value;
+  const myLifts = getLifts.value;
+
+  localStorage.setItem("set-floors",myFloors);
+  localStorage.setItem("set-lifts",myLifts);
+
+  window.location.href = "lift.html";
+})
