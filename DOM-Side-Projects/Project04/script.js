51b951e859e8993bce35b59088b95dacc7daafd8
Pritam Chauhan <rajputpritam619@gmail.com>
2023-02-24 20:58:22 +0530

    project04 started

---
+++ DOM-Side-Projects/Project04/script.js
@@ -0,0 +1,29 @@
+let data;
+
+const inputBox = document.getElementById("inputBox");
+const countryName = document.getElementById("countryName");
+const stateName = document.getElementById("stateName");
+const cityName =document.getElementById("cityName");
+const humidity = document.getElementById("humidity");
+const windSpeed = document.getElementById("windSpeed");
+const temprature = document.getElementById("temprature");
+const logoImage = document.getElementById("logoImage");
+const weatherStatus = document.getElementById("weatherStatus");
+
+const getData = async (event) => {
+   event.preventDefault();
+   if(!inputBox.value){
+    alert("Please Enter the city name:");
+    return
+   }
+
+   //
+   const city = inputBox.value;
+
+   //Fetch Details
+   const fetchData = await fetch(`https://api.weatherapi.com/v1/current.json?key=df78dc8491864e89be791516232402&q=${city}`);
+
+   const orgData = await fetchData.json();
+   data = orgData;
+   console.log(data); 
+};
