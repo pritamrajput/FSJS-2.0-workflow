745d5c31281fa294279eeb3b6d80ad67a12f270f
Pritam Chauhan <rajputpritam619@gmail.com>
2023-03-01 15:37:34 +0530

    few changes done

--- DOM-Side-Projects/Project03/lift.js
+++ DOM-Side-Projects/Project03/lift.js
@@ -43,6 +43,7 @@ function createLifts(liftIndex){
     const lift = document.createElement("div");
     lift.classList.add("lifts",`lift__${liftIndex}`);
     lift.setAttribute("data-state","free");
+    lift.setAttribute("data-floor",1);
     lift.textContent = `lift ${liftIndex + 1}`
     return lift;
 }
@@ -73,15 +74,29 @@ for(let i = 0 ; i < getFloors ;i++){
         getDiv.appendChild(innerDiv);
         const button = [createButton("Down")];
         button[0].addEventListener("click",function(){
+            console.log(getFloors);
+            lifts[liftOrder].setAttribute("data-floor",getFloors);
             if(lifts[liftOrder].getAttribute("data-state") === "free"){
-            lifts[liftOrder].setAttribute("data-state","busy");
-            setTimeout(function(){
-             lifts[liftOrder].setAttribute("data-state","free"); 
-            },3000)
-            lifts[liftOrder].style.bottom = `${(getFloors-1)*120}px`
-            liftOrder += 1;
-        }
+                lifts[liftOrder].setAttribute("data-state","busy");
+                setTimeout(function() {
+                    lifts[liftOrder].setAttribute("data-state","free"); 
+                },2500);
+                lifts[liftOrder].style.bottom = `${(getFloors-1)*120}px`
+            }
+            else if(lifts[liftOrder].getAttribute("data-state") === "busy"){
+                liftOrder+=1;
+                lifts[liftOrder].setAttribute("data-state","busy");
+                setTimeout(function() {
+                    lifts[liftOrder].setAttribute("data-state","free"); 
+                },2500);
+                lifts[liftOrder].style.bottom = `${(getFloors-1)*120}px`
+            }
+          
+            console.log(liftOrder);
         })
+        if(lifts[liftOrder].style.bottom ===`${(getFloors - 1)*120}px` ){
+            console.log("hello world");
+         }
         appendButton(innerDiv,button); 
     }
     else if(i !== 0 && i!== getFloors - 1){
@@ -90,10 +105,16 @@ for(let i = 0 ; i < getFloors ;i++){
         getDiv.appendChild(innerDiv);
         const button = [createButton("Up"),createButton("Down")];
         button[0].addEventListener("click",function(){
-            lifts[0].style.bottom = `${(getFloors - i - 1)*120}px`
+            console.log(getFloors - i);
+            lifts[liftOrder].setAttribute("data-floor",getFloors - i);
+            lifts[liftOrder].style.bottom = `${(getFloors - i - 1)*120}px`
+            
         })
         button[1].addEventListener("click",function(){
-            lifts[0].style.bottom = `${(getFloors - i - 1)*120}px`
+            console.log(getFloors - i);
+            console.log(liftOrder);
+            lifts[liftOrder].setAttribute("data-floor",getFloors - i);
+            lifts[liftOrder].style.bottom = `${(getFloors - i - 1)*120}px`
         })
         appendButton(innerDiv,button);   
     }
@@ -105,7 +126,9 @@ for(let i = 0 ; i < getFloors ;i++){
         appendButton(innerDiv,button);
         // Appending lifts to the innerDiv
         button[0].addEventListener('click',function(){
-            lifts[0].style.bottom = "0"+"px";
+            console.log(1);
+            lifts[liftOrder].setAttribute("data-floor",getFloors - i);
+            lifts[liftOrder].style.bottom = "0"+"px";
         })
         appendLifts(innerDiv,lifts) ;
         console.log(lifts);
