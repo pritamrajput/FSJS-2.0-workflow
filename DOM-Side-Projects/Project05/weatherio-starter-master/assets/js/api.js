5914640a577e9402982d4eb4642c7883e697cbd0
Pritam Chauhan <rajputpritam619@gmail.com>
2023-05-27 22:53:40 +0530

    changes made

--- DOM-Side-Projects/Project05/weatherio-starter-master/assets/js/api.js
+++ DOM-Side-Projects/Project05/weatherio-starter-master/assets/js/api.js
@@ -2,3 +2,15 @@
 
 const api_key = "77e1e7f99dc1336db2078a96a5b2434d";
 
+/**
+ * Fetch data from serve
+ * @param {string} URL API url
+ * @param {Function} callback callback
+ */
+
+
+export const fetchData = function(URL, callback){
+    fetch(`${URL}&appid=${api_key}`)
+    .then(res=>res.json())
+    .then(data=>callback(data))
+}
