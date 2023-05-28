ceb5fe8882f2490a3f682a6605cdc4cb97e300b8
Pritam Chauhan <rajputpritam619@gmail.com>
2023-05-28 16:54:00 +0530

    API configured

--- DOM-Side-Projects/Project05/weatherio-starter-master/assets/js/api.js
+++ DOM-Side-Projects/Project05/weatherio-starter-master/assets/js/api.js
@@ -27,5 +27,14 @@ export const url = {
     },
     reverseGeo(lat, lon){
         return `http://api.openweathermap.org/geo/1.0/reverse?${lat}&${lon}&limit=5`;
+    },
+
+    /**
+     * 
+     * @param {string} query search query example :- "New delhi", "Ahmedabad"
+     * @returns 
+     */ 
+    geo(query){
+        return `http://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=5`;
     }
 }
