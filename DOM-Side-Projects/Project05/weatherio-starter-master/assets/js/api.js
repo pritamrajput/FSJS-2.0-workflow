57c258798c5258872a48387034326b136665344d
Pritam Chauhan <rajputpritam619@gmail.com>
2023-05-28 16:43:23 +0530

    API configured

--- DOM-Side-Projects/Project05/weatherio-starter-master/assets/js/api.js
+++ DOM-Side-Projects/Project05/weatherio-starter-master/assets/js/api.js
@@ -3,7 +3,7 @@
 const api_key = "77e1e7f99dc1336db2078a96a5b2434d";
 
 /**
- * Fetch data from serve
+ * Fetch data from server
  * @param {string} URL API url
  * @param {Function} callback callback
  */
@@ -13,4 +13,19 @@ export const fetchData = function(URL, callback){
     fetch(`${URL}&appid=${api_key}`)
     .then(res=>res.json())
     .then(data=>callback(data))
+}
+
+export const url = {
+    currentWeather(lat, lon){
+        return `https://api.openweathermap.org/data/2.5/weather?${lat}&${lon}&units=metric`;
+    },
+    forecast(lat,lon){
+        return `https://api.openweathermap.org/data/2.5/forecast?${lat}&${lon}&units=metric`;
+    },
+    airPollution(lat, lon){
+        return `http://api.openweathermap.org/data/2.5/air_pollution?${lat}&${lon}`;
+    },
+    reverseGeo(lat, lon){
+        return `http://api.openweathermap.org/geo/1.0/reverse?${lat}&${lon}&limit=5`;
+    }
 }
